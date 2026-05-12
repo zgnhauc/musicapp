50de474f28bbd2a532eb23ecd6ada2327fc4d0aa
Ambrose1812 <157645693+Ambrose1812@users.noreply.github.com>
2026-05-11 23:19:37 -0400

    Update views.py

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+#Ambrose Mcahee
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import LogMessage, Song, Rating, ListeningHistory
 from django.contrib.auth import authenticate, login, logout
